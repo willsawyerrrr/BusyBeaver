a8237a1f6baa5de562825920ab9ce3407eb048e9
willsawyer2609 <wmsawyer2609@gmail.com>
2022-05-21 01:20:32 +1000

    Removed unnecessary transition comments

--- two_card.py
+++ two_card.py
@@ -12,11 +12,6 @@ from double_sided import TuringMachine
 #create the Turing machine
 bbeaver = TuringMachine( 
     { 
-        #Write your transition rules here as entries to a Python dictionary
-        #For example, the key will be a pair (state, character)
-        #The value will be the triple (next state, character to write, move head L or R)
-        #such as ('q0', '1'): ('q1', '0', 'R'), which says if current state is q0 and 1 encountered
-        #then transition to state q1, write a 0 and move head right.
         ("a", "0"): ("b", "1", "R"),
         ("a", "1"): ("b", "1", "L"),
         ("b", "0"): ("a", "1", "L"),
